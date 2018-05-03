11a775005a0a1fed38097c61a5972fdf5f5578b0
Angelinodc71 <angelinodc75@gmail.com>
2018-05-03 10:53:02 +0200

    El ejercicio de Animaciones

---
+++ 001-hello-world.js
@@ -0,0 +1,15 @@
+var elements = [3, 7, 9, 18, 24, 5, 7, 27, 32, 35, 42, 47];
+
+// Retorna la suma de los números que son estrictamente mayores que 18
+function greaterThan18(elements) {
+
+  if (this.age > 18) return elements[i];
+  return 0;
+}
+var total = 0;
+for (i = 0; i < elements.length; i++) {
+ // Instrucciones
+  total = total + elements[i];
+}
+
+document.getElementById("result").innerHTML = greaterThan18(elements);
